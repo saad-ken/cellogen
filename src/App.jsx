a348ea93e2e41becd20376f6000d633f3083966c
saad-ken <saad-ken>
2025-09-01 01:01:54 +0530

    Full cellogen commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Routes>
         {/* Home Page */}
         <Route
-          path="/cellogen"
+          path="/"
           element={
             <>
               <HeroSection />
